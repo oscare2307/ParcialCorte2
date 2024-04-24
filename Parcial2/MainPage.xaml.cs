db72f62d5b5a08c6dfbc8c62aacad2ba46414f0d
oscare2307 <146147610+oscare2307@users.noreply.github.com>
2024-04-24 18:42:49 -0500

    Agregar archivos de proyecto.

---
+++ Parcial2/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿namespace Parcial2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        double CalcularPago(double principal, double TasaInteres, int Años)
+        {
+            // Validar entrada
+            ValidarEntrada(principal, TasaInteres, Años);
+
+            // Calcular la tasa de interés mensual
+            double TasaMensual = TasaInteres / 100 / 12;
+
+            // Calcular el número de pagos
+            int PagosTotales = Años * 12;
+
+            // Aplicar la fórmula de pago del préstamo
+            return (principal * TasaMensual) / (1 - Math.Pow(1 + TasaMensual, -PagosTotales));
+        }
+
+        void ValidarEntrada(double principal, double TasaInteres, int Años)
+        {
+            if (principal < 1000000)
+            {
+                throw new ArgumentException("El monto del préstamo debe ser mínimo de $1.000.000");
+            }
+            if (TasaInteres > 33.09)
+            {
+                throw new ArgumentException("La tasa de interés excede la tasa de usura(33.09%)");
+            }
+            if (Años * 12 < 6)
+            {
+                throw new ArgumentException("El plazo mínimo debe ser de 6 meses");
+            }
+        }
+
+
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            
+
+            {
+                try
+                {
+                    double principal = double.Parse(EntryPrestamo.Text);
+                    double TasaInteres = double.Parse(EntryTasa.Text);
+                    int Años = int.Parse(EntryPeriodo.Text);
+
+                    double mensualidad = CalcularPago(principal, TasaInteres, Años);
+                    EntryPago.Text = $"Pago mensual: ${mensualidad:0.00}";
+                }
+                catch (FormatException ex)
+                {
+                    DisplayAlert("Error de formato", "Ingrese valores numéricos válidos para monto, tasa e interés", "Aceptar");
+                }
+                catch (ArgumentException ex)
+                {
+                    DisplayAlert("Validación de datos", ex.Message, "Aceptar");
+                }
+            }
+
+        }
+
+        private void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            EntryPrestamo.Text = "";
+            EntryTasa.Text = "";
+            EntryPeriodo.Text = "";
+            EntryPago.Text = "";
+            EntryPrestamo.Focus();
+        }
+    }
+
+}
---
+++ Parcial2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Parcial2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
